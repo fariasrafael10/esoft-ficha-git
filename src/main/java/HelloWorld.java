d8783c10e2f70c4bdcfc8c13ec123b6aeefabdd4
fariasrafael10 <122155664+fariasrafael10@users.noreply.github.com>
2025-03-02 14:28:30 +0000

    adicionar meu nome de novo: Rafael

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -4,6 +4,11 @@ public class HelloWorld extends JFrame {
     private JPanel panel1;
     private JLabel lblMensagem;
     private JLabel lblMensagem2 = new JLabel("Eu Rafael estive aqui ");
+    private JLabel lblMensagem3 = new JLabel("Eu Rafael estive aqui de novo");
+    private JLabel lblMensagem4 = new JLabel("Eu Rafael estive aqui de novo");
+
+
+
 
     public HelloWorld() {
         setContentPane(panel1);
